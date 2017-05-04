5f70acf2f7f48ef93f636add0fa284874b8c8f8d
Jonathan Wheaton <jonathan@ourplaceontheweb.org>
2017-05-04 05:24:10 -0400

    finished draft version

--- bin/start-encrypted-disks.sh
+++ bin/start-encrypted-disks.sh
@@ -7,18 +7,26 @@ logfile="$(basename $0)-$(date +%FT%T).log"
 
 # Iterate over the lines in /etc/crypttab.
 get_crypttab_entries() {
+    gawk -- 'BEGIN{FS=" "}$0 !~ /^\s*#/{print $1}' /etc/crypttab
 }
 
 # Iterate over the "noauto" optioned lines in /etc/fstab
 get_fstab_entries() {
+    grep -v "^\s*#" /etc/fstab | gawk -- 'BEGIN{FS=" "}/noauto/{print $2}' -
 }
 
 do_stuff() {
     # start devices in /etc/crypttab.
-    cryptdisks_start enc1
+    for i in get_crypttab_entries; do
+        echo "starting encrypted device ${i}..."
+        cryptdisks_start ${i}
+    done
 
     # mount the "noauto" devices.
-    mount /home2
+    for i in get_fstab_entries; do
+        echo "mounting encrypted device at mount point ${i}..."
+        mount ${i}
+    done
 }
 
 do_stuff 2>&1 | tee $logfile
