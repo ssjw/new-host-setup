58f5e8ee7cc5b11f3293ce45fcceca584b3bdf24
[jwheaton] <[jonathan@ourplaceontheweb.org]>
2018-04-15 07:29:36 -0400

    Fix systemctl command

--- setup-files/usr/local/etc/post-open-encrypted-devices-commands.d/nextcloud/start-services.sh
+++ setup-files/usr/local/etc/post-open-encrypted-devices-commands.d/nextcloud/start-services.sh
@@ -4,6 +4,6 @@ systemctl start mariadb
 systemctl status --no-pager mariadb
 
 systemctl start apache2
-systemctl status --no-pager mariadb
+systemctl status --no-pager apache2
 
 # vim:tw=76:sts=4:sw=4
