a87083ef11cdf41a088542dfc2194633405e094e
[jwheaton] <[jonathan@ourplaceontheweb.org]>
2018-04-15 08:06:40 -0400

    Changing the way services are started

--- setup-files/usr/local/etc/post-open-encrypted-devices-commands.d/nextcloud/start-services.sh
+++
@@ -1,9 +0,0 @@
-# Start the services that depend encrypted devices.
-
-systemctl start mariadb
-systemctl status --no-pager mariadb
-
-systemctl start apache2
-systemctl status --no-pager apache2
-
-# vim:tw=76:sts=4:sw=4
