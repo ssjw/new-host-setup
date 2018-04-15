1ac7a173e17669713b459a2cf627c299d8cbcb1c
[jwheaton] <[jonathan@ourplaceontheweb.org]>
2018-04-15 07:08:09 -0400

    Added new host

---
+++ setup-files/usr/local/etc/post-open-encrypted-devices-commands.d/nextcloud/start-services.sh
@@ -0,0 +1,9 @@
+# Start the services that depend encrypted devices.
+
+systemctl start mariadb
+systemctl status --no-pager mariadb
+
+systemctl start apache2
+systemctl status --no-pager mariadb
+
+# vim:tw=76:sts=4:sw=4
