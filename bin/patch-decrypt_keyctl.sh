5cb6a7d9b96edf5c37102139b4e741131d374423
[jwheaton] <[jonathan@ourplaceontheweb.org]>
2018-04-26 05:30:40 -0400

    New method for decrypt_keyctl

---
+++ bin/patch-decrypt_keyctl.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+logfile="$(basename $0)-$(date +%FT%T).log"
+
+MYDIR_=$(dirname $0)
+
+do_stuff() {
+    pushd /lib/cryptsetup/scripts
+    echo "Patching decrypt_keyctl..."
+    patch decrypt_keyctl $MYDIR_/../setup-files/decrypt_keyctl.patch
+    if [ $? -ne 0]; then
+        die "Error patching /lib/cryptsetup/scripts/decrypt_keyctl. You'd better investigate."
+    fi
+    popd
+}
+
+do_stuff 2&>1 | tee $logfile
+
+# vim:sts=4:sw=4:tw=76
